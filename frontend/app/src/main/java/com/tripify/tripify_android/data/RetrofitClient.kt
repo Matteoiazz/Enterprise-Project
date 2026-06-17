3bae49334a07a31d7349b3e62d783814edd15141
Matteoiazz <matteo.iazzolino@gmail.com>
2026-06-17 15:17:19 +0200

    a

--- frontend/app/src/main/java/com/tripify/tripify_android/data/RetrofitClient.kt
+++ frontend/app/src/main/java/com/tripify/tripify_android/data/RetrofitClient.kt
@@ -1,11 +1,12 @@
 package com.tripify.tripify_android.data
 
+import com.tripify.tripify_android.BuildConfig
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.155:8080"
+    private val BASE_URL = BuildConfig.BASE_URL
 
     // Funzione originale di Dario (NON TOCCARE)
     fun createApi(tokenManager: TokenManager): AuthApi {
